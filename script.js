bf53ee21c3827f6eba104617b1a2946d59a2b0c8
Andresfullstack7 <109389167+Andresfullstack7@users.noreply.github.com>
2023-04-04 22:44:46 -0700

    ALL NEW
    ALL NEW

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const yesBtn = document.querySelector('.yes');
 const noBtn = document.querySelector('.no');
 
 yesBtn.addEventListener('click', function() {
-  message.innerHTML = 'You have made me the happiest person on earth! I love you more than words can express, and I promise to cherish and love you always. You are my soulmate, my partner, and my best friend. Together, we can conquer anything life throws our way.';
+  message.innerHTML = '<p>You have made me the happiest person on earth! I love you more than words can express, and I promise to cherish and love you always. You are my soulmate, my partner, and my best friend. Together, we can conquer anything life throws our way.</p>' +
+  '<img src="./IMG-9128.jpg" alt="Image Description">';
 });
 
 noBtn.addEventListener('click', function() {
